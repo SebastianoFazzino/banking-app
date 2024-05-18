be7a7ecfa946f671486f0298e774c610d1cb4c82
Seb <sebastiano.fazzino@swagyourlife.com>
2024-05-18 19:14:27 +0300

    typo

--- banking-app-frontend/src/app/services/api.service.ts
+++ banking-app-frontend/src/app/services/api.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {LoanRequest} from "../models/loan-request";
 import {Observable} from "rxjs";
 import {LoanResponse} from "../models/loan-response";
-import {environment} from "../../environments.environment";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
